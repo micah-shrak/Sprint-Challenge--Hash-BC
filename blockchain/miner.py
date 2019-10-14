24aa9b00982a980a0fc1bc5062736f70da4ae286
Lavon Stalling <micah.shrak@gmail.com>
2019-10-13 21:33:36 -0700

    blockchain

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -26,6 +26,8 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    while valid_proof(last_proof, proof) is False:
+        proof += random.randin(1, 1)
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,7 +42,14 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+
+    guess_proof = str(proof).encode()
+
+    last_hash = str(last_hash)
+
+    hash_proof = hashlib.sha256(guess_proof).hexdigest()
+
+    return hash_proof[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
